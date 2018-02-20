eafb5ecfa79e0deb08a86211bddf20e3dc920fc5
Calum Mackervoy <c.mackervoy@gmail.com>
2018-02-20 15:13:14 +0000

    adding some data to models
    __str__ functions and Meta classes (with ordering properties)

--- env_dev/rpg/gameapi/models.py
+++ env_dev/rpg/gameapi/models.py
@@ -8,14 +8,29 @@ class ImageContent(models.Model):
     # below: models.URLField()?
     src = models.CharField(max_length=100, null=True)
     priority = models.PositiveSmallIntegerField(default=0)
+    #tells Python how to display a human-readable representation of an object
+    def __str__(self):
+        return self.src
+
+    class Meta:
+        ordering = ['priority']
 
 class TextContent(models.Model):
     text = models.CharField(max_length=255)
     priority = models.PositiveSmallIntegerField(default=0)
+    #tells Python how to display a human-readable representation of an object
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ['priority']
 
 class Option(models.Model):
     text = models.CharField(max_length=255)
     # TODO: some way of linking this option to an action/function
+    #tells Python how to display a human-readable representation of an object
+    def __str__(self):
+        return self.text
 
 class Page(models.Model):
     title = models.CharField(max_length=100, null=True)
@@ -24,3 +39,6 @@ class Page(models.Model):
     content = models.ManyToManyField(TextContent)
     images = models.ManyToManyField(ImageContent)
     options = models.ManyToManyField(Option)
+    #tells Python how to display a human-readable representation of an object
+    def __str__(self):
+        return self.title
